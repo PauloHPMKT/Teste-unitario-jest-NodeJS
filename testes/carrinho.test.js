27c472ec853adcc4ed21eaeea4c987a29b776dc5
Paulo Sergio <pauloserg861@gmail.com>
2022-08-05 20:28:13 -0300

    adicionando testes unitarios com jesty

--- testes/carrinho.test.js
+++ testes/carrinho.test.js
@@ -1,8 +1,12 @@
 import Item from "../item";
 import Carrinho from "../carrinho";
 
-describe('Testar carrinho', () => {
-    it('Deve retornar alguma coisa', () => {
+describe('Teste dos itens', () => {
+    it('Deve retornar 3 campos: nome, valor e quantidade', () => {
+        const item = new Item('Melancia', 2.5, 10);
 
+        expect(item.nome).toBe('Melancia');
+        expect(item.valor).toBe(2.5);
+        expect(item.quantidade).toBe(10);
     });
 });
